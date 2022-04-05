f11a1a26ff3d693fcbce2899320cf110cf8b9151
Ondřej Matura <ondramaturacze@gmail.com>
2022-04-05 22:52:12 +0200

    Initial commit

---
+++ app/src/main/java/cz/zcu/maturao/tetris/GameLoopThread.kt
@@ -0,0 +1,52 @@
+package cz.zcu.maturao.tetris
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.Log
+import android.view.WindowManager
+import java.lang.Exception
+import kotlin.math.roundToLong
+
+class GameLoopThread(private val view: GameView) : Thread() {
+    var running: Boolean = false
+
+    override fun run() {
+        val windowService = view.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val fps = windowService.defaultDisplay.refreshRate
+
+        val ticksPS: Long = 1000 / fps.roundToLong()
+        var startTime: Long
+        var sleepTime: Long
+
+        while (running) {
+            var c: Canvas? = null
+            startTime = System.currentTimeMillis()
+
+            try {
+                c = view.holder.lockCanvas()
+
+                synchronized(view.holder) {
+                    // Pridana kontrola, aby nehazelo chybu pri tlacitku BACK
+                    if (c != null) {
+                        view.onDraw(c)
+                    }
+                }
+            } finally {
+                if (c != null) {
+                    view.holder.unlockCanvasAndPost(c)
+                }
+            }
+
+            sleepTime = ticksPS - (System.currentTimeMillis() - startTime)
+
+            try {
+                if (sleepTime > 0) {
+                    sleep(sleepTime)
+                } else {
+                    sleep(10)
+                }
+            } catch (_: Exception) {
+            }
+        }
+    }
+}
---
+++ app/src/main/java/cz/zcu/maturao/tetris/GameView.kt
@@ -0,0 +1,71 @@
+package cz.zcu.maturao.tetris
+
+import android.content.Context
+import android.graphics.*
+import android.os.Build
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+
+class GameView(context: Context) : SurfaceView(context) {
+    private val gameLoopThread = GameLoopThread(this)
+
+    init {
+        holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                gameLoopThread.running = true
+                gameLoopThread.start()
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                var retry = true
+                gameLoopThread.running = false
+                while (retry) {
+                    try {
+                        gameLoopThread.join()
+                        retry = false
+                    } catch (_: InterruptedException) {
+                    }
+                }
+            }
+        })
+    }
+
+    private var posX = 0f
+    private var posY = 0f
+    private var speedX = 30f
+    private var speedY = 30f
+
+    private val paint = Paint().apply {
+        color = Color.RED
+    }
+
+    public override fun onDraw(canvas: Canvas) {
+        posX += speedX
+        posY += speedY
+
+        val size = 500f
+
+        if (posX !in 0f..(width - size)) {
+            speedX *= -1
+        }
+
+        if (posY !in 0f..(height - size)) {
+            speedY *= -1
+        }
+
+
+        canvas.drawColor(Color.GREEN)
+        canvas.drawRect(posX, posY, posX + size, posY + size, paint)
+
+    }
+}
